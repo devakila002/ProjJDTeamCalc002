e30546679c0022819e4a2c912841625ce6b1b9ea
dev.jaikumar001 <dev.jaikumar001@gmail.com>
2023-08-19 11:38:24 +0530

    Added two Numbers

--- CalcAddTests.Tests/CalcAddTest1.cs
+++ CalcAddTests.Tests/CalcAddTest1.cs
@@ -3,8 +3,13 @@ namespace CalcAddTests.Tests;
 public class CalcAddTest1
 {
     [Fact]
-    public void Test1()
+    public void CheckIf5Plus7Equals12Test()
     {
-
+        //Arrange
+        Calculator myCalc = new Calculator();
+        //Act
+        int sumOf5And7 = myCalc.AddNumbers(5, 7);
+        //Assert
+        Assert.Equal(12, sumOf5And7);
     }
 }
